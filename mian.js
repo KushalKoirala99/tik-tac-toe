0587ca94a9ac8c546fd945f872b748459ab8d75d
kushal <kushalkoirala58@gmail.com>
2025-02-24 13:38:23 +0545

    Feat : Added 2 player

--- mian.js
+++ mian.js
@@ -2,30 +2,41 @@ let gameBoard = (function () {
   let board = [];
   for (let i = 0; i < 3; i++) {
     board[i] = [];
-    for(let j= 0 ; j < 3 ; j++){
-        board[i].push('')
+    for (let j = 0; j < 3; j++) {
+      board[i].push("");
     }
   }
 
   getBoard = () => board;
 
-  let placeMarker = (row,col,marker) => {
-    if(board[row][col] === ""){
-        board[row][col] = marker;
-        return true;
+  let placeMarker = (row, col, marker) => {
+    if (board[row][col] === "") {
+      board[row][col] = marker;
+      return true;
     }
-      return false;
-  }
+    return false;
+  };
 
-  return { getBoard ,placeMarker};
+  return { getBoard, placeMarker };
 })();
 
-
-class Players{
-    constructor(name,marker){
-        this.name = name;
-        this.marker = marker;
-    }
+class Players {
+  constructor(name, marker) {
+    this.name = name;
+    this.marker = marker;
+  }
 }
 
 
+let gameController = (function(){
+ let players = [];
+ players.push(new Players('Player 1' , "X"))
+ players.push(new Players('Player 2' , "O"))
+
+ 
+
+ return {players}
+})()
+
+
+console.log(gameController.players)
